3b494f9ea480b91b6b3b2987ed0b39872b22966d
DEXIN KONG <dkong@us.ibm.com>
2020-03-24 01:15:28 -0400

    draft 1

--- hackassembler.py
+++ hackassembler.py
@@ -5,7 +5,24 @@ from tkinter import filedialog
 
 root = Tk()
 root.fileName = filedialog.askopenfilename (initialdir = "C:/Users/DEXINKONG/Desktop/nand2tetris\projects/06", title = "select source file", filetypes = (("asm files", "*.asm" ), ("All files", "*.*")))
-print(root.fileName)
-fscource = open(root.filName)
-print(fsource)
+filename = str(root.fileName)
+print(filename)
+fsource = open(filename, "r")
+# raw = fsource.read()
+line0=fsource.readlines()
+print(line0.strip())
+fintm = open("intermediate.txt","w+")
+lnum=0
+for line in fsource.readlines():
+    print(line.strip())
+    if line[0]== "//" and line[1] == "//" :
+        print("this line is notes")
+    elif line == "/n" :
+        print("empty line")
+    else :
+        fintm.write(line)
+        lnum+=1
+print("number of code lines: ", lnum)
+fsource.close()
+fintm.close()
 
